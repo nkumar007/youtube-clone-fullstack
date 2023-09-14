64d963e97af10e393346671125fbc1709e7311ef
nkumar007 <naveen.mbuff98@gmail.com>
2023-09-14 17:15:47 +0530

    fetch and render videos

--- yt-web-client/app/page.tsx
+++ yt-web-client/app/page.tsx
@@ -1,12 +1,25 @@
-export default function Home() {
+import Image from "next/image";
+import Link from "next/link";
+import {getVideos} from "./utils/firebase/functions";
+
+export default async function Home() {
+  const videos = await getVideos();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold text-4xl">app/page.tsx</code>
-        </p>
-      </div>
+    <main className="flex gap-4">
+      {videos.map((video) => (
+        <Link href={`/watch?v=${video.filename}`} key={video.id}>
+          <Image
+            src={"/thumbnail.png"}
+            alt="video"
+            width={120}
+            height={80}
+            className="m-4"
+          />
+        </Link>
+      ))}
     </main>
   );
 }
+
+export const revalidate = 30;
